ad7c11268a708ec69d2678472a4defabd32d1a1a
Inshiya Ravat <inshiya.ravat@simformsolutions.com>
2025-04-17 14:44:15 +0530

    feature: generation of result once all input fields are filled

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,41 @@ import ResultTable from "./components/ResultTable/ResultTable";
 import { ChangeEvent, useState } from "react";
 
 type Input = number | undefined;
+type AnnualData= {
+  year : number,
+  investmentValue: number,
+  interest :number,
+  totalInterest :number,
+  investedCapital :number,
+}[]
+function getResult(initialInvestment:number,annualInvestment:number,expectedReturn:number,duration:number){
+  const annualData:AnnualData = [];
+  let investmentValue = initialInvestment;
+
+  for (let i = 0; i < duration; i++) {
+    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+    investmentValue += interestEarnedInYear + annualInvestment;
+    if(i === 0){
+      annualData.push({
+        year: i + 1,
+        investmentValue: Math.ceil(investmentValue), 
+        interest: Math.ceil(interestEarnedInYear), 
+        totalInterest: Math.ceil(interestEarnedInYear), 
+        investedCapital: Math.ceil(initialInvestment+annualInvestment),
+      });
+    }else{
+      annualData.push({
+        year: i + 1,
+        investmentValue: Math.ceil(investmentValue), 
+        interest: Math.ceil(interestEarnedInYear), 
+        totalInterest: Math.ceil(annualData[i-1].interest + interestEarnedInYear), 
+        investedCapital: Math.ceil(annualData[i-1].investedCapital + annualInvestment),
+      });
+    }
+  }
+
+  return annualData;
+}
 
 function App() {
   const [initialInvestment, setInitialInvestment] = useState<Input>(undefined);
@@ -33,7 +68,9 @@ function App() {
   return (
     <>
       <UserInput initialInvestment={initialInvestment} annualInvestment={annualInvestment} expectedReturn={expectedReturn} duration={duration} inputChange={handleUserInputchange}/>
-      <ResultTable />
+      {initialInvestment && annualInvestment && expectedReturn && duration && (
+        <ResultTable resultData={getResult(initialInvestment,annualInvestment,expectedReturn,duration)}/>
+      )}
     </>
   );
 }
--- src/components/ResultTable/ResultTable.tsx
+++ src/components/ResultTable/ResultTable.tsx
@@ -1,4 +1,20 @@
-const ResultTable = () => {
+type AnnualData= {
+    year : number,
+    investmentValue: number,
+    interest :number,
+    totalInterest :number,
+    investedCapital :number,
+  }[]
+interface ResultTableProp {
+    resultData: AnnualData
+}
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
+const ResultTable = ({resultData}:ResultTableProp) => {
   return (
     <table id="result">
       <thead>
@@ -8,6 +24,19 @@ const ResultTable = () => {
         <th>Total Interest</th>
         <th>Invested Capital</th>
       </thead>
+      <tbody>
+        {
+            resultData.map((data)=>(
+                <tr key={data.year}>
+                    <td>{data.year}</td>
+                    <td>{formatter.format(data.investmentValue)}</td>
+                    <td>{formatter.format(data.interest)}</td>
+                    <td>{formatter.format(data.totalInterest)}</td>
+                    <td>{formatter.format(data.investedCapital)}</td>
+                </tr>
+            ))
+        }
+      </tbody>
     </table>
   );
 };
