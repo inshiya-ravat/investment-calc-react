ca4219c402fa356d7917a25afdf4b97212156f71
Inshiya Ravat <inshiya.ravat@simformsolutions.com>
2025-04-17 15:09:06 +0530

    fix: created a component for header and removed modification made in index files

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import '../../index.css'
+
+const Header = () => {
+  return (
+    <header id="header">
+      <img src="/investment-calculator-logo.png" alt="logo"/>
+      <h1>Investment Calculator</h1>
+    </header>
+  )
+}
+
+export default Header
